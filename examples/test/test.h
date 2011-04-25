555cb7095e15cdcf4efb474831d94e09b56f5769
Chris Colbert <sccolbert@gmail.com>
2011-04-25 16:41:05 -0500

    updated renderers to support arbitrary function pointers. Updated test example to use such crazy constructions.

--- examples/test/test.h
+++ examples/test/test.h
@@ -1,18 +1,21 @@
-
+// typdef of simple fundamental type
 typedef long long LLong;
 
 
+// struct containing typedef'd field
 struct Foo {
     int a;
     LLong b;
 };
 
 
+// struct with tag and typedef name
 typedef struct _Bar {
     double b;
 } Bar;
 
 
+// typedef'd anonymous enum
 typedef enum {
     ONE,
     TWO,
@@ -22,12 +25,15 @@ typedef enum {
 } Baz;
 
 
+// Typedef'd typedef
 typedef Baz Spam;
 
 
+// typedef'd struct
 typedef struct Foo Eggs;
 
 
+// union with nested struct
 union AUnion {
     int a;
     double b;
@@ -38,6 +44,7 @@ union AUnion {
 };
 
 
+// struct with nested union
 typedef struct _Astruct {
     Eggs *eggsPtr;
     union _uNested {
@@ -46,6 +53,7 @@ typedef struct _Astruct {
 } Astruct;
 
 
+// function which returns pointer to int
 int *foo_bar(double t, Bar *barptr);
 
 
@@ -57,3 +65,34 @@ double *(*crazy_fn(int*, char*, Baz))[10];
 typedef char *(*(*crazy_fn_ptr_)(void *))[42];
 
 
+// struct with function pointer field
+struct AnotherFoo {
+    int (*func)(double, int*);
+};
+
+
+// function that that takes int and returns pointer to function that takes
+// two floats and returns float
+float (*returns_func_ptr(int foo))(float, float);
+
+
+// function which takes pointer to char and returns pointer to function which 
+// takes int and double and returns pointer to function that takes int and long
+// and returns pointer to function that takes pointer to char and returns 
+// pointer to double.
+double *(*(*(*returns_func_ptr_nested(char*))(int, double))(int, long))(char*);
+
+
+// struct with function pointer field which takes int and returns pointer
+// to function which take float pointer and returns pointer to char
+struct CrazyField {
+    char *(*(*crazy_ptr)(int))(float*);
+};
+
+
+// function pointer variable declaration
+double (*func_ptr_var)(void);
+
+
+// function pointer variable which returns function pointer which returns int
+int (*(*func_ptr_func_ptr_var)(void))(void);
