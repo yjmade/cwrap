b8b6a779f11a63fb137e5db1d696b1a842f8abef
Gregor Thalhammer <gregor.thalhammer@gmail.com>
2013-01-04 20:07:51 +0100

    add libclang frontend to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(name='CWrap',
       version='0.0',
       description='Automatical generate Cython wrappers from C header files',
       packages=['cwrap', 'cwrap.backend', 'cwrap.frontends', 
-                'cwrap.frontends.gccxml']
+                'cwrap.frontends.gccxml',
+                'cwrap.frontends.clang',
+                ]
       )
